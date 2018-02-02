244239283db6f72276463f9b237d2b6e188e6058
loicbourgois <loicbourgois.web@gmail.com>
2018-02-02 16:16:02 +0100

    Use new mail

--- 2.sh
+++ 2.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 git config --global user.name "loicbourgois"
-git config --global user.email "loicbourgois.web@gmail.com"
+git config --global user.email "loic@bourgois.pro"
 git config --global --list
+ssh-keygen -t rsa -b 4096 -C "loic@bourgois.pro"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa

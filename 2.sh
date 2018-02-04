5313925c1204254ec913c8298e4424c8d047cfc7
loicbourgois <loic@bourgois.pro>
2018-02-04 20:30:31 +0100

    Add message for ssh

--- 2.sh
+++ 2.sh
@@ -2,6 +2,8 @@
 git config --global user.name "loicbourgois"
 git config --global user.email "loic@bourgois.pro"
 git config --global --list
+echo '' 
+echo 'Setting ssh for github' 
 ssh-keygen -t rsa -b 4096 -C "loic@bourgois.pro"
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
+ssh-add ~/.ssh/github

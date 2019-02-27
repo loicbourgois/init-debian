ad59c6ef21702a92e8025f04e252b1f685321863
Loïc Bourgois <loic@bourgois.pro>
2019-02-27 19:54:38 +0100

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -4,6 +4,8 @@ git config --global user.email "loic@bourgois.pro"
 git config --global --list
 echo '' 
 echo 'Setting ssh for github' 
+echo 'Use ~/.ssh/github as filename'
+read -p 'Press enter to continue'
 ssh-keygen -t rsa -b 4096 -C "loic@bourgois.pro"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/github

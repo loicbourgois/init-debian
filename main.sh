05c427c8f9abe66723ee392942b1e73edcac0464
Loïc Bourgois <loic@bourgois.pro>
2019-02-27 19:37:12 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,13 +5,16 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 echo 'Edit sources.list'
+echo 'Useful if apt is configured to use a cdrom'
+read -p 'Press enter to continue'
 gedit '/etc/apt/sources.list'
 apt-get install sudo
 #https://unix.stackexchange.com/questions/179954/username-is-not-in-the-sudoers-file-this-incident-will-be-reported/258865#258865
-echo 'Add this line to /etc/sudoers'
+echo 'Add the following line to /etc/sudoers'
 echo 'user        ALL=(ALL)      ALL'
+read -p 'Press enter to continue'
 visudo
 adduser user sudo
 echo 'You can now run ./0.sh'
-echo 'Don t forget to run chmod +x 0.sh'
+echo 'Don\'t forget to run chmod +x 0.sh'
 su user

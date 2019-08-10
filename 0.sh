8e8dff84c8d4ff045bed8352e86702eb97b6debe
loicbourgois <loic@bourgois.pro>
2019-08-10 08:39:18 +0200

    Correctly order clone, cd, pull

--- 0.sh
+++ 0.sh
@@ -3,8 +3,8 @@ sudo apt-get install aptitude
 sudo aptitude install git
 cd ~
 git clone https://github.com/loicbourgois/init-debian.git
-git pull origin master
 cd ~/init-debian
+git pull origin master
 chmod +x ./1.sh
 ./1.sh
 chmod +x ./2.sh
